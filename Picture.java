ee7f25e21c01227ac28f8aea82e6561ec13c551a
Jennifer Puhalka <jpuh0799@stu.raritanval.edu>
2015-09-21 18:32:35 -0400

    Project Updated

--- Picture.java
+++ Picture.java
@@ -11,10 +11,15 @@
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle grass;
-    private Circle sun;
+    private Square background;
+    private Triangle grass1;
+    private Triangle grass2;
+    private Triangle grass3;
+    private Triangle grass4;
+    private Triangle grass5;
+    private Circle bubble1;
+    private Circle bubble2;
+    private Circle bubble3;
     private Person guy;
 
     /**
@@ -31,53 +36,75 @@ public class Picture
     public void draw()
     {
       
-        grass = new Triangle(); 
-        grass.changeColor("green");
-        grass.changeSize(50, 180);
-        grass.moveHorizontal(-40);
-        grass.moveVertical(85);
-        grass.makeVisible();
+        background = new Square();
+        background.changeColor("blue");
+        background.moveHorizontal(-305);
+        background.moveVertical(-150);
+        background.changeSize(450);
+        background.makeVisible();
         
-        grass = new Triangle(); 
-        grass.changeColor("green");
-        grass.changeSize(60, 180);
-        grass.moveHorizontal(50);
-        grass.moveVertical(75);
-        grass.makeVisible();
+        grass1 = new Triangle(); 
+        grass1.changeColor("green");
+        grass1.changeSize(50, 180);
+        grass1.moveHorizontal(-40);
+        grass1.moveVertical(85);
+        grass1.makeVisible();
         
-        grass = new Triangle(); 
-        grass.changeColor("green");
-        grass.changeSize(50, 180);
-        grass.moveHorizontal(150);
-        grass.moveVertical(85);
-        grass.makeVisible();
+        grass2 = new Triangle(); 
+        grass2.changeColor("green");
+        grass2.changeSize(60, 180);
+        grass2.moveHorizontal(50);
+        grass2.moveVertical(75);
+        grass2.makeVisible();
         
-        grass = new Triangle(); 
-        grass.changeColor("green");
-        grass.changeSize(60, 180);
-        grass.moveHorizontal(-150);
-        grass.moveVertical(75);
-        grass.makeVisible();
+        grass3 = new Triangle(); 
+        grass3.changeColor("green");
+        grass3.changeSize(50, 180);
+        grass3.moveHorizontal(150);
+        grass3.moveVertical(85);
+        grass3.makeVisible();
+        
+        grass4 = new Triangle(); 
+        grass4.changeColor("green");
+        grass4.changeSize(60, 180);
+        grass4.moveHorizontal(-130);
+        grass4.moveVertical(75);
+        grass4.makeVisible();
+        
+        grass5 = new Triangle(); 
+        grass5.changeColor("green");
+        grass5.changeSize(90, 180);
+        grass5.moveHorizontal(10);
+        grass5.moveVertical(45);
+        grass5.makeVisible();
 
-        sun = new Circle();
-        sun.changeColor("light blue");
-        sun.moveHorizontal(100);
-        sun.moveVertical(40);
-        sun.changeSize(80);
-        sun.makeVisible();
+        bubble1 = new Circle();
+        bubble1.changeColor("light blue");
+        bubble1.moveHorizontal(10);
+        bubble1.moveVertical(40);
+        bubble1.changeSize(40);
+        bubble1.makeVisible();
+        
+        bubble2 = new Circle();
+        bubble2.changeColor("light blue");
+        bubble2.moveHorizontal(50);
+        bubble2.moveVertical(0);
+        bubble2.changeSize(60);
+        bubble2.makeVisible();
+        
+        bubble3 = new Circle();
+        bubble3.changeColor("light blue");
+        bubble3.moveHorizontal(100);
+        bubble3.moveVertical(-60);
+        bubble3.changeSize(80);
+        bubble3.makeVisible();
         
         guy = new Person();
-        guy.changeColor("red");
-        guy.moveHorizontal(20);
-        guy.moveVertical(20);
+        guy.changeColor("magenta");
+        guy.moveHorizontal(-55);
+        guy.moveVertical(40);
         guy.makeVisible();
         
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(120);
-        window.moveVertical(40);
-        window.changeSize(40);
-        window.makeVisible();
     }
 
     /**
@@ -85,12 +112,18 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        if (wall != null)   // only if it's painted already...
+        if (background != null)   // only if it's painted already...
         {
-            wall.changeColor("black");
-            window.changeColor("white");
-            grass.changeColor("black");
-            sun.changeColor("black");
+            background.changeColor("black");
+            grass1.changeColor("white");
+            grass2.changeColor("white");
+            grass3.changeColor("white");
+            grass4.changeColor("white");
+            grass5.changeColor("white");
+            bubble1.changeColor("white");
+            bubble2.changeColor("white");
+            bubble3.changeColor("white");
+            guy.changeColor("black");
         }
     }
 
@@ -99,12 +132,18 @@ public class Picture
      */
     public void setColor()
     {
-        if (wall != null)   // only if it's painted already...
+        if (background != null)   // only if it's painted already...
         {
-            wall.changeColor("red");
-            window.changeColor("black");
-            grass.changeColor("green");
-            sun.changeColor("yellow");
+            background.changeColor("blue");
+            grass1.changeColor("green");
+            grass2.changeColor("green");
+            grass3.changeColor("green");
+            grass4.changeColor("green");
+            grass5.changeColor("green");
+            bubble1.changeColor("light blue");
+            bubble2.changeColor("light blue");
+            bubble3.changeColor("light blue");
+            guy.changeColor("magenta");
         }
     }
 }
